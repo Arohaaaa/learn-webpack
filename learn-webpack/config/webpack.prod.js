db1753ba0836a3cb3bd87f9bf1185e84db734bcf
DESKTOP-JC97EK5\63268 <632681127@qq.com>
2022-09-25 19:57:29 +0800

    #根据不同环境调整配置#

---
+++ learn-webpack/config/webpack.prod.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const EsLintWebpackPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "static/js/[name].js",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 20 * 1024, // 小于10kb的图片会被base64处理
+                    },
+                },
+                generator: {
+                    filename: "static/images/[hash:8][ext][query]",
+                },
+            },
+            {
+                test: /\.(ttf|woff2?|map4|map3|avi)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "static/media/[hash:8][ext][query]",
+                },
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, // 排除node_modules代码不编译
+                loader: "babel-loader",
+            },
+        ],
+    },
+    plugins: [
+        new EsLintWebpackPlugin({
+            // 指定检查文件的根目录
+            context: path.resolve(__dirname, "../src"),
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "../public/index.html"),
+        }),
+    ],
+    mode: "production",
+};
