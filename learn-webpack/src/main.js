c6f8b1c9905c3991476142fd14cc9daf1f379058
DESKTOP-JC97EK5\63268 <632681127@qq.com>
2022-09-25 18:32:19 +0800

    #处理样式资源# 添加sass、sass-loader处理scss、sass资源

--- learn-webpack/src/main.js
+++ learn-webpack/src/main.js
@@ -1,6 +1,8 @@
 import count from "./js/count";
 import sum from "./js/sum";
 import "./css/index.css";
+import "./sass/index.scss";
+import "./sass/index.sass";
 
 console.log(count(2, 1));
 console.log(sum(1, 2, 3, 4));
