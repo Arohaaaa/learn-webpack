86dfc50880c8b85e01b28e9a329a502b1443c9d2
DESKTOP-JC97EK5\63268 <632681127@qq.com>
2022-09-25 19:28:41 +0800

    #自动清空打包目录#

--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "static/js/[name].js",
+        clean: true,
     },
     module: {
         rules: [
